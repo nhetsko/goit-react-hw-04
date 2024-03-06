38624d3fb64fea41b9f47825f01d944c82e524ca
Nadiia <nadiahecko@gmail.com>
2024-03-06 20:01:19 +0200

    add

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,8 +1,10 @@
 import { Hearts } from 'react-loader-spinner'
-
+import css from './Loader.module.css'
 export default function Loader() {
     return (
-        <Hearts  height="80" width="80" color="#4fa94d"
-  />
+        <div className={css.loader}>  
+         <Hearts  height="80" width="80" color="#4fa94d"/>
+        </div>
+        
     )
 }
